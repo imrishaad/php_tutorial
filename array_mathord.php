c31c85a00409b7710fed62110ba0e5f677fb51a5
Safayetul Rishaad <118076687+imrishaad@users.noreply.github.com>
2023-03-11 10:50:14 +0600

    my first code

---
+++ array_mathord.php
@@ -0,0 +1,12 @@
+<?php
+// echo "<h2>PHP is Fun!</h2>";
+// echo "Hello world!<br>";
+
+
+
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . "."
+
+
+
+?>

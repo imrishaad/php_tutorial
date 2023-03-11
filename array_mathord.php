d43af1d228076d04cafc00ac59f09e8be8cda7cb
Safayetul Rishaad <118076687+imrishaad@users.noreply.github.com>
2023-03-11 10:51:37 +0600

    my changes code

--- array_mathord.php
+++ array_mathord.php
@@ -5,8 +5,9 @@
 
 
 $cars = array("Volvo", "BMW", "Toyota");
-echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . "."
 
-
+foreach ($cars as $key => $car) {
+    print($car);
+}
 
 ?>

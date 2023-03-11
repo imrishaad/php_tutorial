20b3c33ecbc38ac70e575fd5a92d1e4095ac2796
Safayetul Rishaad <118076687+imrishaad@users.noreply.github.com>
2023-03-11 11:04:17 +0600

    edit code

--- array_mathord.php
+++ array_mathord.php
@@ -1,6 +1,7 @@
 <?php
-// echo "<h2>PHP is Fun!</h2>";
-// echo "Hello world!<br>";
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+print
 
 
 
@@ -9,5 +10,24 @@ $cars = array("Volvo", "BMW", "Toyota");
 foreach ($cars as $key => $car) {
     print($car);
 }
+$cars = array("Volvo", "BMW", "Toyota");
+$arrlength = count($cars);
+
+for($x = 0; $x < $arrlength; $x++) 
+  echo $cars[$x];
+  $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach($age as $x => $x_value) 
+  echo "Key=" . $x . ", Value=" . $x_value;
+  echo "<br>";
+  $cars = array (
+    array("Volvo",22,18),
+    array("BMW",15,13),
+    array("Saab",5,2),
+    array("Land Rover",17,15)
+  );
+  $numbers = array(4, 6, 2, 22, 11);
+sort($numbers);
+
 
 ?>
